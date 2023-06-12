fb057c580137b6594c2179e340c08d17cc637def
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2023-06-12 14:09:59 +0200

    Ryddet og op opdelt funktionerne

--- Gruppe-Projekt/datafetch.js
+++ Gruppe-Projekt/datafetch.js
@@ -1,31 +1,54 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=1281&offset=0.";
+let result = "";
+let starters = [];
+let svar = "";
 
-const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.";
-        
-        //her fetcher vi dataen fra URLen og laver til et array
-        async function getStarters() {
-        const response = await fetch(url);
-        const result = await response.json();
-        console.log(result);
-        
-        //her henter vi vores startere ud fra arrayet
-        const starters = [
-        result.results[0].name,
-        result.results[3].name,
-        result.results[6].name,
-        result.results[151].name,
-        result.results[154].name,
-        result.results[157].name
-        ];
-        
-        //her laver vi en liste med starterne
-        const list = document.getElementById("starters"); 
-        //her kører den for hver gang   
-        starters.forEach(function(starter) {
+function gamestart() {
+  getinfo();
+}
 
-        var button = document.createElement("button");
+//her fetcher vi dataen fra URLen og laver til et array
+async function getinfo() {
+  const response = await fetch(url);
+  result = await response.json();
+  console.log(result);
+  getStarters();
+  getStats();
+  makeButtons();
+}
 
-        button.textContent = starter;
+async function getStats() {
+  for (let i = 0; i < starters.length; i++) {
+    const got = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${starters[i]}/`
+    );
+    svar = await got.json();
+    console.log(svar);
+  }
+}
 
-        list.appendChild(button);
-        });
-        }
+async function makeButtons() {
+  //her laver vi en liste med starterne
+  const list = document.getElementById("starters");
+  //her kører den for hver gang
+  starters.forEach(function (starter) {
+    var button = document.createElement("button");
+
+    button.textContent = starter;
+
+    list.appendChild(button);
+  });
+}
+
+async function getStarters() {
+  //her henter vi vores startere ud fra arrayet
+  starters = [
+    result.results[0].name,
+    result.results[3].name,
+    result.results[6].name,
+    result.results[151].name,
+    result.results[154].name,
+    result.results[157].name,
+  ];
+}
+gamestart();
--- Gruppe-Projekt/items.js
+++ Gruppe-Projekt/items.js
@@ -1,56 +1,49 @@
-Inventory = function (){
-    var self ={
-    items: []
-    }    
-    
-    self.addItems  = function(id,amount){
-        for (let i = 0; i < self.items.length; i++) {
-                if (self.items[i].id===id){
-                    self.items[i].amount += amount;
-                    return;
-                }
-            
-        }
-        self.items.push({id:id,amount:amount})
+Inventory = function () {
+  var self = {
+    items: [],
+  };
+
+  self.addItems = function (id, amount) {
+    for (let i = 0; i < self.items.length; i++) {
+      if (self.items[i].id === id) {
+        self.items[i].amount += amount;
+        return;
+      }
     }
-    self.removeItems  = function(id,amount){for (let i = 0; i < self.items.length; i++) {
-        if (self.items[i].id===id){
-            self.items[i].amount -= amount;
-            if (self.items[i].amount <=0)
-            self.items.splice(i,1)
-            return;
-        }
-    
+    self.items.push({ id: id, amount: amount });
+  };
+  self.removeItems = function (id, amount) {
+    for (let i = 0; i < self.items.length; i++) {
+      if (self.items[i].id === id) {
+        self.items[i].amount -= amount;
+        if (self.items[i].amount <= 0) self.items.splice(i, 1);
+        return;
+      }
     }
+  };
+
+  self.hasItems = function (id, amount) {
+    for (let i = 0; i < self.items.length; i++) {
+      if (self.items[i].id === id) {
+        self.items[i].amount >= amount;
+        return false;
+      }
     }
-    
-    self.hasItems  = function(id,amount){
-        for (let i = 0; i < self.items.length; i++) {
-            if (self.items[i].id===id){
-                self.items[i].amount >= amount;
-                return false;
-            }
-    }
-    
-    
-    self.refreshRender  = function(id,amount){}
-    return self
-    }}
-    
-    
-    
-    
-    
-    Item = function(id,name,event){
-        var self ={
-            id:id,
-            name:name,
-            event:event
-        }
-        return self;
-    }
-    
-    Item("portion","portion",function(){
-        player.hp = 10;
-    
-    });
+
+    self.refreshRender = function (id, amount) {};
+    return self;
+  };
+};
+
+Item = function (id, name, event) {
+  var self = {
+    id: id,
+    name: name,
+    event: event,
+  };
+  return self;
+};
+
+Item("portion", "portion", function () {
+  player.hp = 10;
+});
