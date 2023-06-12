f8ab8dfabc10cf6ca5b70faafa3cf42311796acd
Kasper Korthsen <125364229+Kantarellen1@users.noreply.github.com>
2023-06-12 12:09:18 +0200

    Created constructor

--- Gruppe-Projekt/starterShop.js
+++ Gruppe-Projekt/starterShop.js
@@ -1,6 +1,23 @@
-       const url = "https://pokeapi.co/api/v2/pokemon";
-        
+       const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.";
+       
+       
+       
+       class Starters {
+        constructor(value, name, front) {
+          this.value = value;
+          this.name = name;
+          this.front = front;
+        }
+      } 
+      let pokemons = [];
+      var front = document.createElement("img");
         //her fetcher vi dataen fra URLen og laver til et array
+        for (let index = 0; index < arr.length; index++) {
+          pokemons.push(new Starters(value, front))
+          
+        }
+        
+        
         async function getStarters() {
         const response = await fetch(url);
         const result = await response.json();
@@ -8,9 +25,12 @@
         
         //her henter vi vores startere ud fra arrayet
         const starters = [
-        result.results[0].name,
-        result.results[3].name,
-        result.results[6].name
+          result.results[0].name,
+          result.results[3].name,
+          result.results[6].name,
+          result.results[151].name,
+          result.results[154].name,
+          result.results[157].name,
         ];
         
         //her laver vi en liste med starterne
